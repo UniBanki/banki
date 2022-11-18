3b8fd02515224c169d74ac717f8892f91363823e
Denis <d-kasakow@mail.de>
2022-11-18 23:58:14 +0100

    Login Prototyp entwickelt

---
+++ anki/login.js
@@ -0,0 +1,30 @@
+function login(username, password){
+    console.log("login");
+    console.log(username, password);
+
+
+}
+
+function register(username, password){
+    console.log("register");
+    console.log(username, password);
+
+    const user = {username:username, password:password};
+
+    fetch('https://h2992036.stratoserver.net/login.php', {
+        method: 'POST',
+        mode:'cors',
+        cache:'no-cache',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+        .then((response)=> response.json())
+        .then((user)=>{
+            console.log('Succes:', user);
+        })
+        .catch((error)=>{
+            console.error('Error:',error);
+        });
+}
