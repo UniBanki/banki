3ebe887415a8fdb2d488d9cfaf6b04cfc9003b18
Denis <d-kasakow@mail.de>
2022-11-19 16:39:45 +0100

    caching von login.js verhindert und login.js clean gemacht

--- anki/login.js
+++ anki/login.js
@@ -1,15 +1,9 @@
 function login(username, password){
-    console.log("login");
-    console.log(username, password);
-
 
 }
 
 function register(username, password){
-    console.log("register");
-    console.log(username, password);
-
-    const user = {"username":"username", "password":"password"};
+    const user = {"username":username, "password":password};
 
     fetch('https://h2992036.stratoserver.net/register.php', {
         method: 'POST',
