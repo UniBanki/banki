a3c21728da755a8ca6d410a50a0c79a4672886e3
Denis <d-kasakow@mail.de>
2022-11-26 18:39:03 +0100

    Stacks können erstellt werden (nur in GUI)

--- overview/overview.js
+++ overview/overview.js
@@ -1,3 +1,7 @@
+function checkLogin(){
+
+}
+
 function createStack() {
   const innerstacks = document.getElementById("stacks").innerHTML;
 
@@ -5,13 +9,31 @@ function createStack() {
 
   const newstack = `
     <div class="stack">
-        <button>Expand</button>
+        <button onclick="expandStack()">Expand</button>
         <label>${stackname}</label>
-        <button>create card</button>
-        <button>rename</button>
-        <button>delete</button>
+        <button onclick="createCard()">create cards</button>
+        <button onclick="renameStack()">rename</button>
+        <button onclick="deleteStack()">delete</button>
     </div>
     `;
 
   document.getElementById("stacks").innerHTML = newstack+innerstacks;
 }
+
+function expandStack(){
+
+}
+
+function createCard(){
+
+}
+
+function renamestack(){
+    
+}
+
+function deleteStack(){
+    
+}
+
+
