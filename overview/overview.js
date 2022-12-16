9083e119d825adb35838e89ae9d6fa35621ba428
karinavagner112 <wagner_97@mail.ru>
2022-12-16 14:54:22 +0100

    delete und rename funktionen ohne request

--- overview/overview.js
+++ overview/overview.js
@@ -7,17 +7,18 @@ function createStack() {
 
   const stackname = prompt("Name of new stack:");
 
+  if(stackname){
   const newstack = `
-    <div class="stack">
+    <div id="newStack" class="stack">
         <button onclick="expandStack()">Expand</button>
         <label id="stackName">${stackname}</label>
         <button onclick="createCard()">create cards</button>
         <button onclick="renameStack()">rename</button>
-        <button onclick="deleteStack()">delete</button>
+        <button onclick="deleteStack(this)">delete</button>
     </div>
     `;
 
-  document.getElementById("stacks").innerHTML = newstack+innerstacks;
+    document.getElementById("stacks").innerHTML = newstack+innerstacks;}
 }
 
 function expandStack(){
@@ -34,8 +35,14 @@ function renameStack(){
     document.getElementById("stackName").innerHTML = stackrename;
 }
 
-function deleteStack(){
-    
+function deleteStack(item){
+  const stackname = document.getElementById("stackName").innerHTML;
+  if(confirm("delete "+ stackname + "?")){
+  item.parentNode.remove();
+  }
 }
 
+function exit(){
+
+}
 
