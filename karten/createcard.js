58044255048275a34546b41a65036b399a5085da
karinavagner112 <wagner_97@mail.ru>
2023-01-15 21:40:30 +0100

    quill editor + css angepasst

--- karten/createcard.js
+++ karten/createcard.js
@@ -1,39 +1,8 @@
-var editor = null;
-var toolbar = null;
-var buttons = null;
-var contentArea = null;
-var htmlQuestion = null;
-var htmlAnswer = null;
-var visuellView = null;
-
 function screenSize() {
-    editor = document.getElementById('editor');
-    toolbar = document.getElementsByClassName('toolbar')[0];
-    buttons = toolbar.querySelectorAll('.editor-btn');
-    contentArea = editor.getElementsByClassName('content-area')[0];
-    htmlQuestion = contentArea.getElementsByClassName('question')[0];
-    htmlAnswer = contentArea.getElementsByClassName('answer-container')[0];
-    visuellView = contentArea.getElementsByClassName('wysiwyg')[0];
-
-    for (let i = 0; i < buttons.length; i++) {
-        let button = buttons[i];
-    
-        button.addEventListener('click', function (e) {
-            let action = this.dataset.action;
-            switch (action) {
-                case 'toggle-view':
-                    execCodeAction(this, editor);
-                    break;
-                default:
-                    execDefaultAction(action);
-            }
-        });
-    }
-
     console.info(`${screen.width} × ${screen.height}`);
     const editorCont = document.getElementById("editor");
     const questCont = document.getElementById("question");
-    const toolbarCont = document.getElementById("toolbar");
+    const toolbarCont = document.getElementsByClassName("toolbar");
     const answerCont = document.getElementById("answer");
     const header = document.getElementById("headerRight");
 
@@ -56,67 +25,3 @@ function screenSize() {
         editorCont.style.cssText += "margin-top: 4em;";
     }
 }
-
-document.addEventListener('selectionchange', selectionChange);
-
-
-function parentTagActive(elem){
-    if(!elem||!elem.classList||elem.classList.contains('wysiwyg')) return false;
-
-    let toolbarButton;
-
-    let tagName = elem.tagName.toLowerCase();
-    toolbarButton = document.querySelectorAll('.toolbar .editor-btn[data-tag-name="${tagName}"]')[0];
-    if(toolbarButton){
-        toolbarButton.classList.add('active');
-    }
-
-    let textAlign = elem.style.textAlign; 
-    toolbarButton = document.querySelectorAll('.toolbar .editor-btn[data-style="textAlign:${textAlign}"]')[0];
-    if(toolbarButton){
-        toolbarButton.classList.add('active');
-    }
-
-    return parentTagActive(elem.parentNode);
-}
-
-function childOf(child,parent){
-    return parent.contains(child);
-}
-
-function selectionChange(e) {
-    for (let i = 0; i < buttons.length; i++) {
-        let button = null;
-        button = buttons[i];
-
-        if (button.dataset.action == 'toggle-view') continue;
-
-        button.classList.remove('active');
-    }
-
-    if (!childOf(window.getSelection().anchorNode.parentNode, editor)) return false;
-
-    parentTagActive(window.getSelection().anchorNode.parentNode);
-}
-
-function execDefaultAction(action) {
-    document.execCommand(action, false);
-}
-
-function execCodeAction(button, editor) {
-
-    if (button.classList.contains('active')) {
-        visuellView.innerHTML = htmlQuestion.value;
-        htmlQuestion.style.display = 'none';
-        visuellView.style.display = 'block';
-
-        button.classList.remove('active');
-    } else {
-        htmlQuestion.innerText = visuellView.innerHTML;
-        visuellView.style.display = 'none';
-        htmlQuestion.style.display = 'block';
-
-        button.classList.add('active');
-    }
-}
-
