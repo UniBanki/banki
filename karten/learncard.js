c4ee75aa4f811a5dc49af420835b6c0a486423d1
karinavagner112 <wagner_97@mail.ru>
2023-02-16 09:12:17 +0100

    id name korrektur

--- karten/learncard.js
+++ karten/learncard.js
@@ -17,7 +17,24 @@ function createContent(){
           theme: 'snow',
           readOnly: true
     });
+}
 
-    quillQuestion.blur();
-    quillAnswer.blur();
+function screenSize() {
+    console.info(`${screen.width} × ${screen.height}`);
+    const mainContainer = document.getElementById("mainContainer");
+    const questCont = document.getElementById("contentAnswer");
+    const header = document.getElementById("headerRight");
+
+    if (screen.width >= 375 && screen.width <= 414) {
+        header.style.cssText += "margin-top: 1em";
+        mainContainer.style.cssText += "padding-top: 1em; padding-left: 8em;";
+    }
+    else if (screen.width < 374) {
+        header.style.cssText += "margin-top: 1em";
+        mainContainer.style.cssText += "padding: 0; margin-top: 1em; width: 330px;padding-top: 1em;";
+    }
+    else if (screen.width == 768) {
+        header.style.cssText += "margin: -2em";
+        mainContainer.style.cssText += "margin-top: 4em;";
+    }
 }
